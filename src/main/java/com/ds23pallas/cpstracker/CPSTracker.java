d15e4087ef36ad8cde79a3825cc6bd8f14bd45c0
Andy Todd <andy.blacktapes@gmail.com>
2022-04-15 12:25:05 +0200

    Tidying

--- src/main/java/com/ds23pallas/cpstracker/CPSTracker.java
+++ src/main/java/com/ds23pallas/cpstracker/CPSTracker.java
@@ -1,22 +1,12 @@
 package com.ds23pallas.cpstracker;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
 
 @Mod("cpstracker")
 public class CPSTracker {
@@ -24,63 +14,12 @@ public class CPSTracker {
 
     public CPSTracker() {
         final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-
         bus.addListener(this::clientSetup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM CPS TRACKER PREINIT");
-        //LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
         LOGGER.info("HELLO FROM CPS TRACKER CLIENT SETUP");
         CPSTrackerEventHandler.init();
-    }
-
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // Some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.messageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-
-
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents
-    {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
-        {
-            // Register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
-
     }
 
 }
--- src/main/java/com/ds23pallas/cpstracker/CPSTrackerEventHandler.java
+++ src/main/java/com/ds23pallas/cpstracker/CPSTrackerEventHandler.java
@@ -2,7 +2,6 @@ package com.ds23pallas.cpstracker;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
@@ -21,13 +20,15 @@ public class CPSTrackerEventHandler {
     private static CPSTrackerEventHandler cpsTrackerEventHandler;
 
     private int row1 = 20;
-    private int row2 = 40;
+    private int row2 = 42;
+    private int row3 = 64;
     private int col1 = 10;
     private int col2 = 40;
-    private int col3 = 60;
+    private int col3 = 80;
 
     int onColor = 0xFFFFFF;
     int offColor = 0xc0c0c0;
+    int fillColor = 0x80A0A0A0;
     private boolean aIsDown, wIsDown, sIsDown, dIsDown = false;
 
 
@@ -48,7 +49,9 @@ public class CPSTrackerEventHandler {
         drawString(matrix,"S",col2,row2,(sIsDown?onColor:offColor));
         drawString(matrix,"D",col3,row2,(dIsDown?onColor:offColor));
 
-        GuiComponent.fill(matrix,row1,col1,row1+20,col1+20,0x80A0A0A0);
+        drawString(matrix,"LMB",col1,row3,offColor);
+        drawString(matrix,"RMB",col3,row3,offColor);
+
     }
 
     private void onTick(TickEvent.ClientTickEvent e) {
@@ -61,7 +64,8 @@ public class CPSTrackerEventHandler {
     }
 
     private void drawString(PoseStack matrix, String text, int x, int y, int color) {
-        Minecraft.getInstance().font.draw(matrix,text,x,y,color);
+        Minecraft.getInstance().font.draw(matrix,text,x+7,y+7,color);
+        GuiComponent.fill(matrix,x,y,x+20,y+20,fillColor);
     }
 
 }
